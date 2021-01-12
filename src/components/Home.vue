a6961d4893040f5ebf29db39f0f9dbeae7a2c550
Nertil Poci <nertilpoci@hotmail.com>
2021-01-12 11:57:28 +0100

    InitialComit

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<b-container >
+  <b-row align-h="center" align-content="center" >
+    <b-col style="flex-grow:unset" >
+      <LocaleChanger/>
+      </b-col>
+  </b-row>
+   <b-row align-h="center">
+    <b-col>
+       <p>
+     {{$t('message')}}
+    </p>
+    </b-col>
+  </b-row>
+</b-container>
+ 
+</template>
+
+<script>
+import LocaleChanger from './LocaleChanger.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    LocaleChanger
+  }
+}
+</script>
---
+++ src/components/LocaleChanger.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-card
+    title="Language"
+    :img-src="getImgUrl($i18n.locale)"
+    img-alt="Image"
+    img-top
+    class="mb-2"
+        style="max-width: 200px;"
+
+  >
+     <b-form-select     style="max-width: 200px;"
+ v-model="$i18n.locale">
+      <b-form-select-option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</b-form-select-option>
+    </b-form-select>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'locale-changer',
+  data () {
+    return { langs: ['en','de'] }
+  }, 
+  methods:{
+    getImgUrl(pic) {
+    var images = require.context('../assets/', false, /\.png$/)
+    return images('./' + pic + ".png")
+}
+  }
+}
+</script>
